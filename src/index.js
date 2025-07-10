83419799edc6fb9aa5ce5c126ec0a44c6994af34
finntastic <179311210+finntaztic@users.noreply.github.com>
2025-07-10 08:22:38 +0800

    redo html

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 import "./styles.css";
+
+const prevBtn = document.querySelector('.prev');
+console.log(prevBtn);
+
+// prevBtn.addEventListener ('click', () => {
+//     console.log('hello');
+//     const box = document.querySelector('.box');
+
+// });
+
+let image = 0;
+showImage(image);
+
+function showImage (n){
+    let images = document.getElementsByTagName('img');
+    console.log(images);
+}
